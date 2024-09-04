95770b42a4ea5c7d7b9a307582f5132c1a891962
simonsionsom <simon19653@gmail.com>
2024-09-04 14:07:43 +0200

    prøver det her med mere pause

--- PreMade/Firkant.py
+++ PreMade/Firkant.py
@@ -41,25 +41,25 @@ while tændt > 0 :
   #din mor
 
 if retning == "l":
+  sleep(1)
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
   sleep(duration)
   print(arlo.stop())
   print("Finished")
   sleep(1)
 elif retning == "d" :
+  sleep(1)
   print("For højre: 1 |For venstre: 0")
   drejning = int(input())
   if drejning == 1:
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
     sleep(duration)
-    print(arlo.stop())
     print("Finished")
     sleep(1)
   elif drejning == 0:
     print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
     sleep(duration)
 
-    print(arlo.stop())
     print("Finished")
     sleep(1)
   else:
@@ -69,6 +69,7 @@ elif retning == "d" :
     sleep(1)
 
 else :
+  sleep(1)
   print("surt show sunnyboy")
   print(arlo.stop())
   print("Finished")
