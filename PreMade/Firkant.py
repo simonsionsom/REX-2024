82867ee2851a86a90fd8d9744625ea16093fd4a3
simonsionsom <simon19653@gmail.com>
2024-09-04 13:44:23 +0200

    prøver lige den her igen

--- PreMade/Firkant.py
+++ PreMade/Firkant.py
@@ -2,7 +2,7 @@ from time import sleep
 
 import robot
 
-
+# Create a robot object and initialize
 arlo = robot.Robot()
 tændt = 0 
 print("Running ...")
@@ -13,23 +13,18 @@ if hastighed == "j":
   leftSpeed = int(input())
   print("Right wheel: ")
   rightSpeed = int(input())
-  print("Drej: d | Eller ligeud: l")
-  retning = input()
-  print("Duration: ")
-  duration = float(input())
 elif hastighed == "n" :
   print("Sætter fart til standard lige")
   leftSpeed = 58
   rightSpeed = 64
-  print("Drej: d | Eller ligeud: l")
-  retning = input()
-  print("Duration: ")
-  duration = float(input())
 else :
   print("Så stopper legen fister")
   print(arlo.stop())
 
-
+print("Drej: d | Eller ligeud: l")
+retning = input()
+print("Duration: ")
+duration = float(input())
 
 while tændt > 0 :
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
