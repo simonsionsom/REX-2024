77913508b259e41d7052b4dd982d164b9dc5c518
simonsionsom <simon19653@gmail.com>
2024-09-04 12:50:47 +0200

    Nemmere at teste om du vil dreje eller køre ligeud og hastighed og sådan

--- PreMade/Firkant.py
+++ PreMade/Firkant.py
@@ -6,14 +6,23 @@ import robot
 arlo = robot.Robot()
 tændt = 0 
 print("Running ...")
+print("Vil du justere hastighed?: j/n")
+hastighed = input()
+if hastighed == "j":
+  print("Left wheel: ")
+  leftSpeed = int(input())
+  print("Right wheel: ")
+  rightSpeed = int(input())
+else :
+  print("Sætter fart til standard lige")
+  leftSpeed = 58
+  rightSpeed = 64
 print("Drej: d | Eller ligeud: l")
 retning = input()
-print("Left wheel: ")
-leftSpeed = int(input())
-print("Right wheel: ")
-rightSpeed = int(input())
+
 print("Duration: ")
 duration = float(input())
+
 while tændt > 0 :
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
   sleep(3)
@@ -25,14 +34,21 @@ while tændt > 0 :
   print(arlo.stop())
   tændt = tændt -1
   #din mor
+
 if retning == "l":
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
   sleep(duration)
 elif retning == "d" :
-  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-  sleep(duration)
+  print("For højre: 1 |For venstre: 0")
+  drejning = int(input())
+  if drejning == 1:
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+    sleep(duration)
+  else :
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    sleep(duration)
 else :
-  print("sur show sunnyboy")
+  print("surt show sunnyboy")
 
 
 
