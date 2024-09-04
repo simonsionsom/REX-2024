a7d4e6b50a7322515148594fd171bede969f6833
KristianJelsted <kristianjelsted@gmail.com>
2024-09-04 12:52:36 +0200

    heyy

--- PreMade/Firkant.py
+++ PreMade/Firkant.py
@@ -6,23 +6,34 @@ import robot
 arlo = robot.Robot()
 tændt = 0 
 print("Running ...")
-leftSpeed = 64
-rightSpeed = 64
+print("Drej: d | Eller ligeud: l")
+retning = input()
+print("Left wheel: ")
+leftSpeed = int(input())
+print("Right wheel: ")
+rightSpeed = int(input())
+print("Duration: ")
+duration = float(input())
 while tændt > 0 :
-  
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
   sleep(3)
   print(arlo.stop())
   # Wait a bit before next command
   sleep(0.041)
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-  sleep(2)
+  sleep(1)
   print(arlo.stop())
   tændt = tændt -1
   #din mor
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-sleep(1)
-print(arlo.stop())
+if retning == "l":
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+  sleep(duration)
+elif retning == "d" :
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+  sleep(duration)
+else :
+  print("sur show sunnyboy")
+
 
 
 
