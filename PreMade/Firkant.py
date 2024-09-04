22b5606702f80529981a2485ea8dc76b0ec1a168
simonsionsom <simon19653@gmail.com>
2024-09-04 12:12:24 +0200

    hvor langt kører dne på et sekund og uden stop

--- PreMade/Firkant.py
+++ PreMade/Firkant.py
@@ -9,20 +9,19 @@ print("Running ...")
 leftSpeed = 64
 rightSpeed = 64
 while tændt > 0 :
-  
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
   sleep(3)
   print(arlo.stop())
   # Wait a bit before next command
   sleep(0.041)
   print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-  sleep(2)
+  sleep(1)
   print(arlo.stop())
   tændt = tændt -1
   #din mor
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
-sleep(1)
-print(arlo.stop())
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+sleep(3)
+
 
 
 
