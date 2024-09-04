c9b668e1d80e589367760438585741c03fc2ac21
simonsionsom <simon19653@gmail.com>
2024-09-04 11:40:34 +0200

    Lad os se hvad en firkant kan ing?

---
+++ PreMade/Firkant.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+tændt = 8 
+print("Running ...")
+while 2> 0 :
+  leftSpeed = 64
+  rightSpeed = 64
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+  sleep(3)
+  print(arlo.stop())
+  leftSpeed = 64
+  rightSpeed = 32
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+  sleep(3)
+  print(arlo.stop())
+  tændt =-1
+
+
+
+
