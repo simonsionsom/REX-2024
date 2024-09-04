79ee6811791fbf30fe6598b3806f3fab3d6aac2b
KristianJelsted <kristianjelsted@gmail.com>
2024-09-04 13:07:05 +0200

    jepp

--- PreMade/letsdoit.py
+++ PreMade/letsdoit.py
@@ -9,12 +9,12 @@ print("Running ...")
 
 
 # send a go_diff command to drive forward
-leftSpeed = 58
+leftSpeed = 32
 rightSpeed = 64
 print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 # Wait a bit while robot moves forward
 
-sleep(3)
+sleep(2)
 
 # send a stop command
 print(arlo.stop())
@@ -31,48 +31,5 @@ sleep(3)
 # send a stop command
 print(arlo.stop())
 
-# Wait a bit before next command
 sleep(0.041)
-
-
-
-# request to read Front sonar ping sensor
-print("Front sensor = ", arlo.read_front_ping_sensor())
-sleep(0.041)
-
-
-# request to read Back sonar ping sensor
-print("Back sensor = ", arlo.read_back_ping_sensor())
-sleep(0.041)
-
-# request to read Right sonar ping sensor
-print("Right sensor = ", arlo.read_right_ping_sensor())
-sleep(0.041)
-
-# request to read Left sonar ping sensor
-print("Left sensor = ", arlo.read_left_ping_sensor())
-sleep(0.041)
-
-
-
-leftSpeed = 58
-rightSpeed = 64
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-
-# Wait a bit while robot moves forward
-sleep(3)
-
-# send a stop command
-print(arlo.stop())
-
-# Wait a bit before next command
-sleep(0.041)
-
-# send a go_diff command to drive backwards the same way we came from
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 0))
-
-# Wait a bit while robot moves backwards
-sleep(3)
-
-print(arlo.stop())
 print("Finished")
