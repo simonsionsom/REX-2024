24082b739fa8338351f0e075b3b60644f73fbd59
Flameguard <147167983+Flameguard@users.noreply.github.com>
2024-09-04 15:56:14 +0200

    variable sat ind

--- PreMade/First_run.py
+++ PreMade/First_run.py
@@ -23,28 +23,32 @@ def drej(sekunder, h_left, h_right):
 
 sekunder_drej = 0.75
 sekunder_lige = 2.5
+hastighed_kør_r = 64
+hastighed_kør_l = 63
+hastighed_drej_l = 35.5
+hastighed_drej_r = 35.5
 
 print("Jytte, mindre press på speederen! ...")
 sleep(1)
 
 for rotation in range(1, 4):
 
-    lige_ud(sekunder_lige, 63, 64)
+    lige_ud(sekunder_lige, hastighed_kør_l, hastighed_kør_r)
 
-    drej(sekunder_drej, 63, 63.5)
+    drej(sekunder_drej, hastighed_drej_l, hastighed_drej_r)
 
-    lige_ud(sekunder_lige, 63, 64)
+    lige_ud(sekunder_lige, hastighed_kør_l, hastighed_kør_r)
 
-    drej(sekunder_drej, 63, 63.5)
-
-
-    lige_ud(sekunder_lige, 63, 64)
-
-    drej(sekunder_drej, 63, 63.5)
+    drej(sekunder_drej, hastighed_drej_l, hastighed_drej_r)
 
 
-    lige_ud(sekunder_lige, 63, 64)
+    lige_ud(sekunder_lige, hastighed_kør_l, hastighed_kør_r)
 
-    drej(sekunder_drej, 63, 63.5)
+    drej(sekunder_drej, hastighed_drej_l, hastighed_drej_r)
+
+
+    lige_ud(sekunder_lige, hastighed_kør_l, hastighed_kør_r)
+
+    drej(sekunder_drej, hastighed_drej_l, hastighed_drej_r)
 
 print(arlo.stop())
