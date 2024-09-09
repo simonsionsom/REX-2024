caa5e19bf0db534b249af9cdd937eba2d10d67eb
Flameguard <147167983+Flameguard@users.noreply.github.com>
2024-09-09 16:39:05 +0200

    prøv det her lort

---
+++ PreMade/lolnuvirkerdet.py
@@ -0,0 +1,25 @@
+sekunder_drej = 0.55
+sekunder_lige = 3
+hastighed_kør_r = 63
+hastighed_kør_l = 63
+hastighed_drej_l = 35
+hastighed_drej_r = 35
+
+
+from time import sleep
+import robot
+
+
+arlo = robot.Robot()
+
+print("Kører lige ud: ")
+print(arlo.go_diff(hastighed_kør_l, hastighed_kør_r, 1, 1))
+sleep(3)
+
+print("Drejer med hastighederne: ")
+print("Venstre: {h_left}, Højre: {h_right}")
+print("I {sekunder_drej} sekunder")
+sleep(0.041)
+print(arlo.go_diff(hastighed_drej_l, hastighed_drej_r, 1, 0))
+sleep(0.55)
+
