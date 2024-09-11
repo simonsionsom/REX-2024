4b765256dafe1f4f3a248fabd3fc979b0d7e846d
simonsionsom <simon19653@gmail.com>
2024-09-11 11:11:42 +0200

    tester sensorRun

--- PreMade/sensorRun.py
+++ PreMade/sensorRun.py
@@ -7,6 +7,21 @@ arlo = robot.Robot()
 
 tændt = True
 
+Front = 0
+Left = 0
+Right = 0
+Behind = 0
+
+def Scan(F,L,R,B):
+  F = arlo.read_front_ping_sensor()
+  L = arlo.read_left_ping_sensor()
+  R = arlo.read_right_ping_sensor()
+  B = arlo.read_back_ping_sensor()
+  return F, L, R, B
+
 while tændt:
-  
+  Front, Left, Right, Behind = Scan(Front,Left,Right,Behind)
+  print(Front)
+  print(Right, Left)
   sleep(2)
+  tændt = False
