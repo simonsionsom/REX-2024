5336017c4a1b6f971e839df5c5ca6b5d8d57d64f
simonsionsom <simon19653@gmail.com>
2024-09-11 11:31:48 +0200

    gik han amok på rundkørslen?

--- PreMade/sensorRun.py
+++ PreMade/sensorRun.py
@@ -5,12 +5,13 @@ import robot
 # Create a robot object and initialize
 arlo = robot.Robot()
 
-tændt = True
+tændt = 0
 
 Front = 0
 Left = 0
 Right = 0
 Behind = 0
+
 
 def Scan(F,L,R,B):
   F = arlo.read_front_ping_sensor()
@@ -19,9 +20,24 @@ def Scan(F,L,R,B):
   B = arlo.read_back_ping_sensor()
   return F, L, R, B
 
-while tændt:
+while tændt <3:
+  stop = False
   Front, Left, Right, Behind = Scan(Front,Left,Right,Behind)
-  print(Front)
-  print(Right, Left)
+  sleep(0.41)
+  print(arlo.go_diff(63,63,1,1))
   sleep(2)
-  tændt = False
+  if Front < 1000 & stop == False:
+    print(arlo.go_diff(45, 46, 1, 0))
+    sleep(1)
+    stop = True
+  if stop == True:
+    Front, Left, Right, Behind = Scan(Front,Left,Right,Behind)
+    if Front > Behind:
+      print(arlo.go_diff(63,63,1,1))
+      sleep(2)
+    else: 
+      print(arlo.go_diff(63,63,0,0))
+    print(arlo.go_diff(45,46,0,1))
+    sleep(1)
+  tændt= tændt + 1
+
