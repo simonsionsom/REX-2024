58f39b9a0d669e0410b57606ffb28a90c06203f4
Nicolaj <nicolaj1@yahoo.dk>
2024-09-11 15:27:06 +0200

    ping test

---
+++ PreMade/task2.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+import robot
+
+arlo = robot.Robot()
+
+print("Starting")
+while True:
+    front = arlo.read_front_ping_sensor()
+    sleep(0.041)
+    left = arlo.read_left_ping_sensor()
+    sleep(0.041)
+    right = arlo.read_right_ping_sensor()
+    sleep(0.041)
+    back = arlo.read_back_ping_sensor()
+    print(f"Front: {front}")
+    # print(f"Left: {left}")
+    # print(f"Right: {right}")
+    # print(f"Back: {back}")
+    sleep(3)
