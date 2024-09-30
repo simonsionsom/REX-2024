9e146883b281d10028a0f955036a7bb44dc0684c
simonsionsom <simon19653@gmail.com>
2024-09-30 16:52:27 +0200

    test 123 go go go

--- PreMade/Week4/grid.py
+++ PreMade/Week4/grid.py
@@ -46,8 +46,8 @@ occupancy_map = np.zeros(grid_size, dtype=bool)
 
 def update_occupancy_map(center_x, center_y, distance):
     # Convert the distance and center coordinates to grid indices
-    grid_x = int(center_x / grid_resolution)
-    true_dist = int(distance)
+    grid_x = int(center_x * grid_resolution)
+    true_dist = int(distance* grid_resolution)
     '''grid_y = int(center_y / grid_resolution)
     radius = int(distance / grid_resolution)'''
     print('Her er grid x: ',grid_x)
