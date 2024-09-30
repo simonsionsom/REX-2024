86d2a4d2b2ec19eb0a8f881175884ecc65aec8d7
KristianJelsted <kristianjelsted@gmail.com>
2024-09-30 16:33:26 +0200

    wfagsongsogns

--- localplanning_rrt/grid_occ.py
+++ localplanning_rrt/grid_occ.py
@@ -6,58 +6,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class GridOccupancyMap(object):
+    """
+
+    """
     def __init__(self, low=(0, 0), high=(2, 2), res=0.05) -> None:
-        self.map_area = [low, high]    # A rectangular area    
-        self.map_size = np.array([high[0] - low[0], high[1] - low[1]])
+        self.map_area = [low, high]    #a rectangular area    
+        self.map_size = np.array([high[0]-low[0], high[1]-low[1]])
         self.resolution = res
 
-        # Number of grid cells in each direction
-        self.n_grids = [int(s // res) for s in self.map_size]
+        self.n_grids = [ int(s//res) for s in self.map_size]
 
-        # Create an empty grid
         self.grid = np.zeros((self.n_grids[0], self.n_grids[1]), dtype=np.uint8)
 
-        # Extent for plotting
         self.extent = [self.map_area[0][0], self.map_area[1][0], self.map_area[0][1], self.map_area[1][1]]
 
-    def draw_grid(self):
+    def in_collision(self, pos):
         """
-        Draw the grid system with coordinates
+        find if the position is occupied or not. return if the queried pos is outside the map
         """
-        fig, ax = plt.subplots()
+        indices = [int((pos[i] - self.map_area[0][i]) // self.resolution) for i in range(2)]
+        for i, ind in enumerate(indices):
+            if ind < 0 or ind >= self.n_grids[i]:
+                return 1
+        
+        return self.grid[indices[0], indices[1]] 
 
-        # Draw vertical lines (x-grid)
-        for i in range(self.n_grids[0] + 1):
-            x = self.map_area[0][0] + i * self.resolution
-            ax.plot([x, x], [self.map_area[0][1], self.map_area[1][1]], color='black', linewidth=0.5)
-
-        # Draw horizontal lines (y-grid)
-        for j in range(self.n_grids[1] + 1):
-            y = self.map_area[0][1] + j * self.resolution
-            ax.plot([self.map_area[0][0], self.map_area[1][0]], [y, y], color='black', linewidth=0.5)
-
-        # Add labels to each grid cell
+    def populate(self, n_obs=6):
+        """
+        generate a grid map with some circle shaped obstacles
+        """
+        origins = np.random.uniform(
+            low=self.map_area[0] + self.map_size[0]*0.2, 
+            high=self.map_area[0] + self.map_size[0]*0.2, 
+            size=(n_obs, 2))
+        radius = np.random.uniform(low=0.1, high=0.3, size=n_obs)
+        #fill the grids by checking if the grid centroid is in any of the circle
         for i in range(self.n_grids[0]):
             for j in range(self.n_grids[1]):
-                x_center = self.map_area[0][0] + i * self.resolution + self.resolution / 2
-                y_center = self.map_area[0][1] + j * self.resolution + self.resolution / 2
-                ax.text(x_center, y_center, f"({i}, {j})", ha='center', va='center', fontsize=6)
+                centroid = np.array([self.map_area[0][0] + self.resolution * (i+0.5), 
+                                     self.map_area[0][1] + self.resolution * (j+0.5)])
+                for o, r in zip(origins, radius):
+                    if np.linalg.norm(centroid - o) <= r:
+                        self.grid[i, j] = 1
+                        break
 
-        # Set the aspect ratio to 'equal' so the grid isn't distorted
-        ax.set_aspect('equal')
 
-        # Set the axis limits and labels
-        ax.set_xlim(self.map_area[0][0], self.map_area[1][0])
-        ax.set_ylim(self.map_area[0][1], self.map_area[1][1])
-        ax.set_xlabel("X-coordinate")
-        ax.set_ylabel("Y-coordinate")
-
-        # Show the grid
-        plt.show()
+    
+    def draw_map(self):
+        #note the x-y axes difference between imshow and plot
+        plt.imshow(self.grid.T, cmap="Greys", origin='lower', vmin=0, vmax=1, extent=self.extent, interpolation='none')
 
 if __name__ == '__main__':
     map = GridOccupancyMap()
-    
+    map.populate()
+
     plt.clf()
-    map.draw_grid()
-     
+    map.draw_map()
+    plt.show()
+
+        
