54c63d0eab1eb6cbadc41fdab91c6f4a8f38d7ab
simonsionsom <simon19653@gmail.com>
2024-10-02 21:43:36 +0200

    måske er der faktisk nu ing

--- PreMade/Week3/marker.py
+++ PreMade/Week3/marker.py
@@ -94,7 +94,7 @@ def find_Lengths(corners):
     for i in range(len(corners)):
         rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, real_marker_height, intrinsic_matrix, distortion_coeffs)
         print(tvecs)
-        dist = np.array(tvecs.T[0][0][0],tvecs.T[2][0][0])
+        dist = np.array((tvecs.T[0][0][0],tvecs.T[2][0][0]))
         print(dist)
         dist = dist/ resolution
         np.append(distances,dist)
