e6df8f67f83a289b1da039269b69585817e1d8bc
simonsionsom <simon19653@gmail.com>
2024-10-02 20:27:42 +0200

    allo?

--- PreMade/Week3/marker.py
+++ PreMade/Week3/marker.py
@@ -72,6 +72,7 @@ while cv2.waitKey(4) == -1:
         for i in range(len(ids)):
             print("Object ID = ", ids[i], ", Distance = ", tvecs[i], ", angles = ", rvecs[i])
         cv2.aruco.drawDetectedMarkers(image, corners, ids)
+    print(tvecs)
     z = tvecs.T[2][0]*100
     x = tvecs.T[0][0]*100
     print(f'Here is z:{z}\n And here is x:{x}')
