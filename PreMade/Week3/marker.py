28576b7a91ec524d8ce430df687c78dda424949c
KristianJelsted <kristianjelsted@gmail.com>
2024-10-02 11:58:32 +0200

    sfafasgas

--- PreMade/Week3/marker.py
+++ PreMade/Week3/marker.py
@@ -63,6 +63,9 @@ while cv2.waitKey(4) == -1:
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
+    for i in range(len(ids)):
+        print("Object ID = ", ids[i], ", Distance = ", tvecs[i], ", angles = ", rvecs[i])
+        
     rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, real_marker_height, intrinsic_matrix, distortion_coeffs)
     cv2.aruco.drawDetectedMarkers(image, corners, ids)
     print(rvecs)
