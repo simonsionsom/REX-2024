36ba7568c966e298d7f15c78a183b9d506dd1144
KristianJelsted <kristianjelsted@gmail.com>
2024-10-02 12:29:45 +0200

    sfgagasga

--- PreMade/Week3/marker.py
+++ PreMade/Week3/marker.py
@@ -63,12 +63,13 @@ while cv2.waitKey(4) == -1:
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
     rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, real_marker_height, intrinsic_matrix, distortion_coeffs)
+
     for i in range(len(ids)):
         print("Object ID = ", ids[i], ", Distance = ", tvecs[i], ", angles = ", rvecs[i])
-
+    
     cv2.aruco.drawDetectedMarkers(image, corners, ids)
     print(rvecs)
-    draw_aruco_objects
+    draw_aruco_objects(image, intrinsic_matrix, distortion_coeffs, rvecs, tvecs, real_marker_height)
     resized_image = cv2.resize(image, (320, 240))
     cv2.setWindowProperty(WIN_RF, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
     cv2.imshow(WIN_RF, resized_image)
