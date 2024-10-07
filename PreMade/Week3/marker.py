3ae6d0a081880695d65b6f3455b5bb6f20a815e4
simonsionsom <simon19653@gmail.com>
2024-10-07 16:18:24 +0200

    printer gridSize her

--- PreMade/Week3/marker.py
+++ PreMade/Week3/marker.py
@@ -90,6 +90,7 @@ def populate(boxes):
                     if np.linalg.norm(centroid*gridSize - o) <= radius:
                         print(f'vi gjorde det her er normen {np.linalg.norm(centroid*gridSize - o)}')
                         grid[i+49, j] = 1
+                        print(f'{gridSize}')
                         print(f'Her er x: {i} og her er y: {j}')
                         break
 
