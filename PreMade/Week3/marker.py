4f759d072ba424e041039dd9de61ebb4565c74e5
simonsionsom <simon19653@gmail.com>
2024-10-15 17:01:23 +0200

    endnu en lilel fejl fikset

--- PreMade/Week3/marker.py
+++ PreMade/Week3/marker.py
@@ -95,7 +95,7 @@ def populate(boxes):
 
 def find_Lengths(corners,ids):
     distances = []
-    for i in range(corners):
+    for i in range(0, len(corners)):
         rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], real_marker_height, intrinsic_matrix, distortion_coeffs)
         dist = np.array((tvecs.T[0][0][0]*100,tvecs.T[2][0][0]*100))
         #print(f'Her er tvec{tvecs.T},\n Her er distancen så ing {dist}')
