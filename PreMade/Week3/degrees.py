4095940868cf9c3d0f9589a9f8cc6e3a5578c603
KristianJelsted <kristianjelsted@gmail.com>
2024-10-21 16:20:51 +0200

    dasdsada

--- PreMade/Week3/degrees.py
+++ PreMade/Week3/degrees.py
@@ -9,10 +9,14 @@ def rotate():
     rightSpeed = 32
     print("120 degree ....")
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))  # Spin in one direction
-    sleep(2)
+    sleep(0.41)
+    print(arlo.stop())
+    sleep(0.041)
     print("2 degree ...")
     print(arlo.go_diff(64, 32, 0, 1))
     sleep(1)
+    print(arlo.stop())
+    sleep(0.041)
     print("3 degree ...")
     print(arlo.go_diff(32, 32, 1, 0))
     sleep(0.41)
