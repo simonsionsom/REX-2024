bc108bc7807a5c67f86843f60c8b10f63142ac22
Nicolaj <Nicolaj1@yahoo.dk>
2024-10-28 15:09:59 +0100

    robot selflocalize test

--- PreMade/python/robotselflocalize.py
+++ PreMade/python/robotselflocalize.py
@@ -311,8 +311,8 @@ try:
             # Har ikke fået testet på 2 landmarks med denne her kode. Fjern 314 og 335 for at ændre koden tilbage hvis ikke det her virker
             # og udkommenter 313 og 334 :)
             for p in particles:
-                # particle_weights[p] = 1.0
-                particle_weights[p] = 0.0  # Initialize with neutral weight
+                particle_weights[p] = 1.0
+                # particle_weights[p] = 0.0  # Initialize with neutral weight
 
                 for obj_id, info in detected_objects.items():
                     # Get measured distance and corresponding angle
@@ -332,8 +332,8 @@ try:
                     weight_angle = compute_weight(measured_angle, angle_difference, sigma_angle)
 
                     combined_weight = weight_distance * weight_angle
-                    # particle_weights[p] *= weight_distance * weight_angle
-                    particle_weights[p] += combined_weight
+                    particle_weights[p] *= weight_distance * weight_angle
+                    # particle_weights[p] += combined_weight
 
             # Normalize the particle weights so they sum to 1
             total_weight = sum(particle_weights.values())
