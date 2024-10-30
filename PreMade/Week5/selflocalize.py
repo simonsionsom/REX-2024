1a22e8dd2a661e0deb472395b31664093601ed20
simonsionsom <simon19653@gmail.com>
2024-10-30 14:28:50 +0100

    fjernede angle calculations

--- PreMade/Week5/selflocalize.py
+++ PreMade/Week5/selflocalize.py
@@ -8,7 +8,7 @@ import sys
 import math
 import pile
 import robot
-import angle_calculations
+
 # Flags
 showGUI = True  # Whether or not to open GUI windows
 onRobot = True  # Whether or not we are running on the Arlo robot
