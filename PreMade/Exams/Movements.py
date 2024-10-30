49ec9dbf2be090e43bb578a5305fa714f5383807
simonsionsom <simon19653@gmail.com>
2024-10-30 16:45:40 +0100

    for manuelt input

--- PreMade/Exams/Movements.py
+++ PreMade/Exams/Movements.py
@@ -1,6 +1,78 @@
-import MapCreation
+from time import sleep
 
-newMap = MapCreation.Map()
-newMap.fillMap()
-newMap.showMap()
-newMap.drawRobot((40,20))
+import PreMade.robot as robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+tændt = 4 
+print("Running ...")
+print("Vil du justere hastighed?: j/n")
+hastighed = input()
+if hastighed == "j":
+  print("Left wheel: ")
+  leftSpeed = float(input())
+  print("Right wheel: ")
+  rightSpeed = float(input())
+elif hastighed == "n" :
+  print("Sætter fart til standard lige")
+  leftSpeed = 58
+  rightSpeed = 64
+else :
+  print("Så stopper legen fister")
+  print("Finished")
+  print(arlo.stop())
+  sleep(1)
+
+print("Drej: d | Eller ligeud: l")
+retning = input()
+print("Duration: ")
+duration = float(input())
+
+'''while tændt > 0 :
+  print(arlo.go_diff(63, 63, 1, 1))
+  sleep(3)
+  print(arlo.stop())
+  # Wait a bit before next command
+  sleep(0.041)
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+  sleep(duration)
+  print(arlo.stop())
+  tændt = tændt -1
+  #din mor'''
+
+if retning == "l":
+  sleep(1)
+  print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+  sleep(duration)
+  print(arlo.stop())
+  print("Finished")
+  sleep(1)
+elif retning == "d" :
+  sleep(1)
+  print("For højre: 1 |For venstre: 0")
+  drejning = int(input())
+  if drejning == 1:
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+    sleep(duration)
+    print("Finished")
+    sleep(1)
+  elif drejning == 0:
+    print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+    sleep(duration)
+
+    print("Finished")
+    sleep(1)
+  else:
+    print("Så stopper legen fister")
+    print("Finished")
+    print(arlo.stop())
+    sleep(1)
+
+else :
+  sleep(1)
+  print("surt show sunnyboy")
+  print(arlo.stop())
+  print("Finished")
+  sleep(1)
+
+
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 For at køre ligeud skal højre hjul være 64 og venstre skal være 58.
 
 45 venstre hjul og 46 højre i et sekund
+
+
+
+
+for at lave et kort brug: 
+
+import MapCreation
