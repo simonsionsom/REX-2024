7efe05f724ba913feb297f76fed78a9315f51ec9
KristianJelsted <kristianjelsted@gmail.com>
2024-11-02 14:10:31 +0100

    dsdasdasdsa

---
+++ PreMade/Exams/Movements.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+import robot
+
+arlo = robot.Robot()
+
+print("Left wheel: ")
+leftSpeed = float(input())
+print("Right wheel: ")
+rightSpeed = float(input())
+
+def lige_ud(seconds, left, right): 
+    print("Kører lige ud: ")
+    print(arlo.go_diff(left, right, 1, 1))
+    print(f"Distance traveled {seconds/2}")
+    sleep(seconds)
+
+def drej(seconds, left, right):
+    print("Drejer med hastighederne: ")
+    print("Venstre: {left}, Højre: {right}")
+    print("I {seconds} sekunder")
+    sleep(0.041)
+    print(arlo.go_diff(left, right, 1, 0))
+    sleep(seconds)
