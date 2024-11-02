9af1cc04eaa0c626d9b58943c76c42ec114dd7a6
Flameguard <147167983+Flameguard@users.noreply.github.com>
2024-11-02 14:10:54 +0100

    kun med 2

--- PreMade/Week5/selflocalize.py
+++ PreMade/Week5/selflocalize.py
@@ -124,8 +124,8 @@ landmarkIDs = [3, 4]
 landmarks = {
     3: (0.0, 0.0),  # Coordinates for landmark 1
     4: (300.0, 0.0),  # Coordinates for landmark 2
-    5: (300.0, 400.0),
-    6: (0.0, 400.0)
+    #5: (300.0, 400.0),
+    #6: (0.0, 400.0)
 }
 
 landmark_colors = [CRED, CGREEN, CBLACK, CYELLOW] # Colors used when drawing the landmarks
@@ -308,8 +308,8 @@ try:
         robot_measured_distances = {
             3: 400.0,  # Example distance to landmark 1
             4: 300.0,   # Example distance to landmark 2
-            5: 250.0,
-            6: 200.0
+            #5: 250.0,
+            #6: 200.0
             
 
         }
