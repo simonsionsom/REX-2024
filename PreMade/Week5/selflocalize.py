9f4f030adc01fa37a855b6b5d44a47133ad168c5
simonsionsom <simon19653@gmail.com>
2024-11-03 13:11:44 +0100

    rasmus modsat (ændret partikkel antal)

--- PreMade/Week5/selflocalize.py
+++ PreMade/Week5/selflocalize.py
@@ -218,7 +218,7 @@ try:
 
 
     # Initialize particles
-    num_particles = 1000
+    num_particles = 320
     particles = initialize_particles(num_particles)
 
     est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
