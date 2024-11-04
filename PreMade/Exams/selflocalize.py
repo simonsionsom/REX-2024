a4e92c844c228e7854832a2e6867b15af0dab971
Flameguard <147167983+Flameguard@users.noreply.github.com>
2024-11-04 11:51:37 +0100

    sds

--- PreMade/Exams/selflocalize.py
+++ PreMade/Exams/selflocalize.py
@@ -11,7 +11,7 @@ import robot
 import Movements
 
 # Flags
-showGUI = True  # Whether or not to open GUI windows
+showGUI = False  # Whether or not to open GUI windows
 onRobot = True  # Whether or not we are running on the Arlo robot
 
 def drive_straight(MIN_SPEED=30, DEFAULT_LEFT_SPEED=58, DEFAULT_RIGHT_SPEED=64, diff_angle=0):
@@ -428,6 +428,7 @@ try:
             else:
                 visited.append(landmarkIDs[n]) 
                 print(f"n = {n}")
+                n+=1
                 
         else:
             move.drej(1.0)
@@ -435,7 +436,6 @@ try:
         if landmarkIDs[n] in visited:
 
             move.drej(2.5)
-            n+=1
             print(f"n = {n}")
         
                 
